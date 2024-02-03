680456c411d48c4219c9168e031d10708d73c7ac
Romi Julianto <romyjulians@gmail.com>
2024-02-03 09:34:40 +0700

    feat: add titile

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import { lusitana } from './ui/fonts';
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
+      <title>Acme Dashboard</title>
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
         <AcmeLogo />
       </div>
